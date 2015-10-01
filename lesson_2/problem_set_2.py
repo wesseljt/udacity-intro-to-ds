d9a458ea55f075e2efba02802677d48e6a0d4237
John Wessel <wesseljt@gmail.com>
2015-10-01 15:04:34 -0400

    problem set 2 - over half way!

---
+++ lesson_2/problem_set_2.py
@@ -0,0 +1,125 @@
+import csv
+
+def fix_turnstile_data(filenames):
+    '''
+    Filenames is a list of MTA Subway turnstile text files. A link to an example
+    MTA Subway turnstile text file can be seen at the URL below:
+    
+    
+    As you can see, there are numerous data points included in each row of the
+    a MTA Subway turnstile text file. 
+
+    You want to write a function that will update each row in the text
+    file so there is only one entry per row. A few examples below:
+    A002,R051,02-00-00,05-28-11,00:00:00,REGULAR,003178521,001100739
+    A002,R051,02-00-00,05-28-11,04:00:00,REGULAR,003178541,001100746
+    A002,R051,02-00-00,05-28-11,08:00:00,REGULAR,003178559,001100775
+    
+    Write the updates to a different text file in the format of "updated_" + filename.
+    For example:
+        1) if you read in a text file called "turnstile_110521.txt"
+        2) you should write the updated data to "updated_turnstile_110521.txt"
+
+    The order of the fields should be preserved. Remember to read through the 
+    Instructor Notes below for more details on the task. 
+    
+    In addition, here is a CSV reader/writer introductory tutorial:
+
+    
+    You can see a sample of the turnstile text file that's passed into this function
+    and the the corresponding updated file in the links below:
+    
+    Sample input file:
+
+    Sample updated file:
+
+    '''
+    for name in filenames:
+        f_in = open(name,'r')
+        f_out = open("updated_" + name, 'w')
+        reader_in = csv.reader (f_in, delimiter = ',')
+        writer_out = csv.writer(f_out, delimiter = ',')
+
+        for line in reader_in:
+            length = len(line)
+            group = (length-3)/5
+            ids = line[0],line[1],line[2]
+            for i in range(group):
+                line_output = [line[0],line[1],line[2], line[3+(5*i)], line[4+(5*i)], line[5+(5*i)], line[6+(5*i)], line[7+(5*i)]]
+                writer_out.writerow(line_output)
+    f_in.close()
+    f_out.close()
+
+
+    def create_master_turnstile_file(filenames, output_file):
+    '''
+    Write a function that takes the files in the list filenames, which all have the 
+    columns 'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn', and consolidates
+    them into one file located at output_file.  There should be ONE row with the column
+    headers, located at the top of the file. The input files do not have column header
+    rows of their own.
+    
+    For example, if file_1 has:
+    line 1 ...
+    line 2 ...
+    
+    and another file, file_2 has:
+    line 3 ...
+    line 4 ...
+    line 5 ...
+    
+    We need to combine file_1 and file_2 into a master_file like below:
+     'C/A, UNIT, SCP, DATEn, TIMEn, DESCn, ENTRIESn, EXITSn'
+    line 1 ...
+    line 2 ...
+    line 3 ...
+    line 4 ...
+    line 5 ...
+    '''
+    with open(output_file, 'w') as master_file:
+        master_file.write('C/A,UNIT,SCP,DATEn,TIMEn,DESCn,ENTRIESn,EXITSn\n')
+        for filename in filenames:
+             f = open(filename, mode = 'rU')
+             fl = f.readlines()
+             master_file.writelines(fl)
+
+
+
+import pandas
+
+def get_hourly_entries(df):
+    '''
+    The data in the MTA Subway Turnstile data reports on the cumulative
+    number of entries and exits per row.  Assume that you have a dataframe
+    called df that contains only the rows for a particular turnstile machine
+    (i.e., unique SCP, C/A, and UNIT).  This function should change
+    these cumulative entry numbers to a count of entries since the last reading
+    (i.e., entries since the last row in the dataframe).
+    
+    More specifically, you want to do two things:
+       1) Create a new column called ENTRIESn_hourly
+       2) Assign to the column the difference between ENTRIESn of the current row 
+          and the previous row. If there is any NaN, fill/replace it with 1.
+    
+    You may find the pandas functions shift() and fillna() to be helpful in this exercise.
+    
+    Examples of what your dataframe should look like at the end of this exercise:
+    
+           C/A  UNIT       SCP     DATEn     TIMEn    DESCn  ENTRIESn    EXITSn  ENTRIESn_hourly
+    0     A002  R051  02-00-00  05-01-11  00:00:00  REGULAR   3144312   1088151                1
+    1     A002  R051  02-00-00  05-01-11  04:00:00  REGULAR   3144335   1088159               23
+    2     A002  R051  02-00-00  05-01-11  08:00:00  REGULAR   3144353   1088177               18
+    3     A002  R051  02-00-00  05-01-11  12:00:00  REGULAR   3144424   1088231               71
+    4     A002  R051  02-00-00  05-01-11  16:00:00  REGULAR   3144594   1088275              170
+    5     A002  R051  02-00-00  05-01-11  20:00:00  REGULAR   3144808   1088317              214
+    6     A002  R051  02-00-00  05-02-11  00:00:00  REGULAR   3144895   1088328               87
+    7     A002  R051  02-00-00  05-02-11  04:00:00  REGULAR   3144905   1088331               10
+    8     A002  R051  02-00-00  05-02-11  08:00:00  REGULAR   3144941   1088420               36
+    9     A002  R051  02-00-00  05-02-11  12:00:00  REGULAR   3145094   1088753              153
+    10    A002  R051  02-00-00  05-02-11  16:00:00  REGULAR   3145337   1088823              243
+    ...
+    ...
+
+    '''
+    df['ENTRIESn_hourly'] = (((df.ENTRIESn))-(df.ENTRIESn.shift(1))).fillna(1)
+    return df
