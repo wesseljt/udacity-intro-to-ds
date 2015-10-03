ab0bee0b4c1c2306b0f69c108c5b90b7baf66216
John Wessel <wesseljt@gmail.com>
2015-10-03 13:17:33 -0400

    lesson 3

--- lesson_3/lesson_3.py
+++ lesson_3/lesson_3.py
@@ -47,3 +47,61 @@ def compare_averages(filename):
     # RETURN T TEST RESULTS PRECEDED WITH A TRUE OR FALSE
     print a
     return a
+
+#  gradient descent -- Not really sure about this yet
+import numpy
+import pandas
+
+def compute_cost(features, values, theta):
+    """
+    Compute the cost of a list of parameters, theta, given a list of features 
+    (input data points) and values (output data points).
+    """
+    m = len(values)
+    sum_of_square_errors = numpy.square(numpy.dot(features, theta) - values).sum()
+    cost = sum_of_square_errors / (2*m)
+
+    return cost
+
+def gradient_descent(features, values, theta, alpha, num_iterations):
+    """
+    Perform gradient descent given a data set with an arbitrary number of features.
+    """
+
+    # Write code here that performs num_iterations updates to the elements of theta.
+    # times. Every time you compute the cost for a given list of thetas, append it 
+    # to cost_history.
+    # See the Instructor notes for hints. 
+    m = len(values)
+    cost_history = []
+    for i in range (num_iterations):
+        predicted_values= numpy.dot(features,theta)
+        theta = theta - alpha / m * numpy.dot((predicted_values-values),features)
+        cost = compute_cost(features,values,theta)
+        cost_history.append(cost)
+
+    ###########################
+    ### YOUR CODE GOES HERE ###
+    ###########################
+
+    return theta, pandas.Series(cost_history) # leave this line for the grader
+
+# R squared stuff -- still not really sure about this something to do with testing our model
+
+import numpy as np
+
+def compute_r_squared(data, predictions):
+    # Write a function that, given two input numpy arrays, 'data', and 'predictions,'
+    # returns the coefficient of determination, R^2, for the model that produced 
+    # predictions.
+    # 
+    # Numpy has a couple of functions -- np.mean() and np.sum() --
+    # that you might find useful, but you don't have to use them.
+
+    SST = (( data - np.mean(data))**2).sum()
+    SSReg = (( predictions - data)**2).sum()
+    r_squared = 1-SSReg /SST
+
+    return r_squared
+
+
