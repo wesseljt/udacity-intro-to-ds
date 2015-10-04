daae278c20f25622828a75df71dc0538049a1206
John Wessel <wesseljt@gmail.com>
2015-10-04 17:37:42 -0400

    problem set 3 - half way!

--- lesson_3/lesson_3_problem_set.py
+++ lesson_3/lesson_3_problem_set.py
@@ -31,3 +31,46 @@ def entries_histogram(turnstile_weather):
     r['ENTRIESn_hourly'].hist() # your code here to plot a historgram for hourly entries when it is raining
     #turnstile_weather['...'] # your code here to plot a historgram for hourly entries when it is not raining
     return plt
+
+import numpy as np
+import scipy
+import scipy.stats
+import pandas
+
+def mann_whitney_plus_means(turnstile_weather):
+    '''
+    This function will consume the turnstile_weather dataframe containing
+    our final turnstile weather data. 
+    
+    You will want to take the means and run the Mann Whitney U-test on the 
+    ENTRIESn_hourly column in the turnstile_weather dataframe.
+    
+    This function should return:
+        1) the mean of entries with rain
+        2) the mean of entries without rain
+        3) the Mann-Whitney U-statistic and p-value comparing the number of entries
+           with rain and the number of entries without rain
+    
+    You should feel free to use scipy's Mann-Whitney implementation, and you 
+    might also find it useful to use numpy's mean function.
+    
+    Here are the functions' documentation:
+    http://docs.scipy.org/doc/scipy/reference/generated/scipy.stats.mannwhitneyu.html
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
+    
+    You can look at the final turnstile weather data at the link below:
+    https://www.dropbox.com/s/meyki2wl9xfa7yk/turnstile_data_master_with_weather.csv
+    '''
+    
+    r = turnstile_weather[turnstile_weather.rain==1]
+    nr = turnstile_weather[turnstile_weather.rain==0]
+    
+    with_rain_mean = r.ENTRIESn_hourly.mean()
+    without_rain_mean = nr.ENTRIESn_hourly.mean()
+    
+    mwt = scipy.stats.mannwhitneyu(r.ENTRIESn_hourly, nr.ENTRIESn_hourly, use_continuity=True)
+    
+    U = mwt[0]
+    p = mwt[1]
+    
+    return with_rain_mean, without_rain_mean, U, p # leave this line for the grader
